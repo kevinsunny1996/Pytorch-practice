dd605bdc3c04539883f467f803d22b30edd7bb3a
Kevin Parasseril <kevinsunny1996@gmail.com>
2021-09-09 23:04:31 +0530

    Added arg flags and main method

--- MNIST_Image_classification_convnet/main.py
+++ MNIST_Image_classification_convnet/main.py
@@ -68,3 +68,72 @@ def test(model, device, test_loader):
         test_loss, correct, len(test_loader.dataset), 100. * correct / len(test_loader.dataset)
     ))
 
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description = 'Pytorch MNIST Classifier')
+    parser.add_argument('--batch-size', type = int, default = 64, metavar = 'N',
+                        help = 'input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type = int, default = 1000, metavar = 'N',
+                        help = 'input batch size for testing (default: 1000)')                    
+    parser.add_argument('--epochs', type = int, default = 14, metavar = 'N',
+                        help = 'Number of epochs to training (default: 14)')
+    parser.add_argument('--lr', type = float, default = 1.0, metavar = 'LR',
+                        help = 'Learning rate to be used (default: 1.0)')
+    parser.add_argument('--gamma', type = float, default = 0.7, metavar = 'M',
+                        help = 'Learning rate step gamma (default: 0.7)')
+    parser.add_argument('--no-cuda', action = 'store_true', default = False,
+                        help = 'Disables CUDA training')
+    parser.add_argument('--dry-run', action = 'store_true', default = False,
+                        help = 'Helps to quickly check a single pass')
+    parser.add_argument('--seed', type = int, default = 1, metavar = 'S',
+                        help = 'Random seed (default: 1)')
+    parser.add_argument('--log-interval', type = int, default = 10, metavar = 'N',
+                        help = 'How many batches to wait before logging status (default: 10)')
+    parser.add_argument('--save-model', action = 'store_true', default = False,
+                        help = 'To save the current model')
+
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device('cuda' if use_cuda else 'cpu')
+
+    train_kwargs = {'batch_size' : args.batch_size}
+    test_kwargs = {'batch_size' : args.test_batch_size}
+
+    if use_cuda:
+        cuda_kwargs = {
+            'num_workers' : 1,
+            'pin_memory' : True,
+            'shuffle': True
+        }
+        train_kwargs.update(cuda_kwargs)
+        test_kwargs.update(cuda_kwargs)
+
+    transform = transforms.Compose([
+        transform.ToTensor(),
+        transform.Normalize((0.1307,), (0.3081,))
+    ])
+
+    dataset1 = datasets.MNIST('../data', train = True, download = true, transform = transform)
+    dataset2 = datasets.MNIST('../data', train = false, transform = transform)
+
+    train_loader = torch.utils.data.DataLoader(dataset1, **train_kwargs)
+    test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
+
+    model = Net().to(device)
+    optimizer =  optim.Adadelta(model.parameters(), lr = args.lr)
+
+    scheduler = StepLR(optimizer, step_size = 1, gamma = args.gamma)
+
+    for epoch in range(1, args.epochs+1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(model, device, test_loader)
+        scheduler.step()
+
+    if args.save_model:
+        torch.save(model.state_dict(), 'mnist_cnn.pt')
+
+if __name__ == '__main__':
+    main()
